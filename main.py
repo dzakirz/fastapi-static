edcc4153812318f6be15d889edbdb3e11ca651a9
Dzakirz <dzakirz.dev@gmail.com>
2022-05-13 18:55:57 +0700

    upload

---
+++ main.py
@@ -0,0 +1,51 @@
+from uuid import UUID
+from fastapi import FastAPI, HTTPException
+from models import User, UserUpdateRequest
+from static_db import Database
+
+app = FastAPI()
+
+db = Database.static
+
+@app.get('/')
+async def root():
+  return {
+    "name" : "Dzaki Rozaan"
+  }
+  
+@app.get('/api/v1/users')
+async def fetch_users():
+  return db
+
+@app.post('/api/users')
+async def register_user(user: User):
+  db.append(user)
+  return {"id": user.id}
+
+@app.put('/api/users/{user_id}')
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+  for user in db:
+    if user_update.first_name is not None:
+      user.first_name = user_update.first_name
+    if user_update.last_name is not None:
+      user.last_name = user_update.last_name
+    if user_update.middle_name is not None:
+      user.middle_name = user_update.middle_name
+    if user_update.roles is not None:
+      user.roles = user_update.roles
+    return f"userid: {user.id} updated"
+  raise HTTPException(
+    status_code = 404,
+    detail = f"{user_id} does not exists"
+  )
+
+@app.delete('/api/users/{user_id}')
+async def delete_user(user_id: UUID):
+  for user in db:
+    if user.id == user_id:
+      db.remove(user)
+      return f"{user.first_name} deleted"
+  raise HTTPException(
+    status_code = 404,
+    detail = f"{user_id} does not exists"
+  )
---
+++ static_db.py
@@ -0,0 +1,24 @@
+from typing import List
+from uuid import UUID
+
+from models import Gender, Role, User
+
+
+class Database:
+  static :List[User] = [
+    User(
+      id = UUID("07427c4b-879e-45af-97bd-87398130cf09"),
+      first_name = "Dzaki",
+      last_name = "Rozaan",
+      gender = Gender.male,
+      roles = [Role.student,Role.admin]
+    ),
+    User(
+      id = UUID("84a76d08-0d50-447c-92ca-6b033daf2c45"),
+      first_name = "Nathan",
+      middle_name = "Anandala",
+      last_name = "Panjaitan",
+      gender = Gender.male,
+      roles = [Role.student]
+    )
+]
